8dcf43edcc04c08fc781a3fde2c65d7f66e5137d
sammous <moustachir.sami@gmail.com>
2014-10-12 00:47:58 +0200

    add Beacon

---
+++ Beacon/Beacon/ViewController.m
@@ -0,0 +1,107 @@
+//
+//  ViewController.m
+//  Beacon
+//
+//  Created by Christopher Ching on 2013-11-28.
+//  Copyright (c) 2013 AppCoda. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Create a NSUUID object
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6D"];
+    
+    // Initialize the Beacon Region
+    self.myBeaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
+                                                                  major:1
+                                                                  minor:1
+                                                             identifier:@"com.appcoda.testregion"];
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.myBeaconRegion.notifyOnEntry=YES;
+    self.myBeaconRegion.notifyOnExit=YES;
+    self.myBeaconRegion.notifyEntryStateOnDisplay=YES;
+    [self.locationManager startMonitoringForRegion:self.myBeaconRegion];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonClicked:(id)sender {
+    
+    // Get the beacon data to advertise
+    self.myBeaconData = [self.myBeaconRegion peripheralDataWithMeasuredPower:nil];
+    
+    // Start the peripheral manager
+    self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self
+                                                                     queue:nil
+                                                                   options:nil];
+}
+
+-(void)peripheralManagerDidUpdateState:(CBPeripheralManager*)peripheral
+{
+    if (peripheral.state == CBPeripheralManagerStatePoweredOn)
+    {
+        // Bluetooth is on
+        
+        // Update our status label
+        self.statusLabel.text = @"Broadcasting...";
+        
+        // Start broadcasting
+        [self.peripheralManager startAdvertising:self.myBeaconData];
+    }
+    else if (peripheral.state == CBPeripheralManagerStatePoweredOff)
+    {
+        // Update our status label
+        self.statusLabel.text = @"Stopped";
+        
+        // Bluetooth isn't on. Stop broadcasting
+        [self.peripheralManager stopAdvertising];
+    }
+    else if (peripheral.state == CBPeripheralManagerStateUnsupported)
+    {
+        self.statusLabel.text = @"Unsupported";
+    }
+}
+
+- (void) locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region
+{
+    [self.locationManager requestStateForRegion:self.myBeaconRegion];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region
+{
+    
+    self.labelState.text=@"Entered region";
+    UILocalNotification* localNotification = [[UILocalNotification alloc] init];
+    localNotification.alertBody = @"New Promo at 20m";
+    
+}
+
+-(void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
+{
+    self.labelState.text=@"Exited region";
+}
+
+-(void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region
+{
+    if(beacons.count>0)
+    {}
+}
+@end
